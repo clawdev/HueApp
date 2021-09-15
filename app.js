48efc914c214fe3d8826a85205a986e97d14a7f1
clawdev <65185751+clawdev@users.noreply.github.com>
2021-09-15 20:03:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Function to turn on lights
 
 function changeStateOn() {
-    return axios.put('https://192.168.0.230/api/03AwTenStl1oqdBBMqWJIQd9mY9cp-mARJUUrIOt/lights/1/state', {"on":true, "sat":250, "bri":250,"hue":2000});
+    return axios.put('https://192.168.0.230/api/7kMWhVbulfgbU6A7ltuJEZidPxEvg7NVzcAJ0nbl/lights/1/state', {"on":true, "sat":250, "bri":250,"hue":2000});
 }
 
 
 // Function to turn off lights
 
 function changeStateOff() {
-    return axios.put('https://192.168.0.230/api/03AwTenStl1oqdBBMqWJIQd9mY9cp-mARJUUrIOt/lights/1/state', {"on":false, "sat":250, "bri":250,"hue":2000});
+    return axios.put('https://192.168.0.230/api/7kMWhVbulfgbU6A7ltuJEZidPxEvg7NVzcAJ0nbl/lights/1/state', {"on":false, "sat":250, "bri":250,"hue":2000});
 }
 
